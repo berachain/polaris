5789a35befce60a755a341a72826597449233af9
Devon Bear <itsdevbear@berachain.com>
2023-01-10 22:36:44 -0500

    Update core/state/utils/utils.go
    Co-authored-by: Cal Bera <calbera@berachain.com>
    Signed-off-by: Devon Bear <itsdevbear@berachain.com>

--- core/state/utils/utils.go
+++ core/state/utils/utils.go
@@ -19,7 +19,7 @@ import (
 	"unsafe"  //#nosec: G702 // yolo
 )
 
-// UnsafeStrToBytes uses unsafe to convert string into byte array. Returned bytes
+// `UnsafeStrToBytes` uses unsafe to convert string into byte array. Returned bytes
 // must not be altered after this function is called as it will cause a segmentation fault.
 func UnsafeStrToBytes(s string) []byte {
 	var buf []byte
