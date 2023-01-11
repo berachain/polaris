f82d1edd44d710f01d0cb9c247f6ef1871bd9f5e
Dev Bear <itsdevbear@berachain.com>
2023-01-10 20:46:01 -0500

    comment

--- common/address.go
+++ common/address.go
@@ -5,10 +5,12 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// `AccAddressToEthAddress` converts a Cosmos SDK `AccAddress` to an Ethereum `Address`.
 func AccAddressToEthAddress(accAddress sdk.AccAddress) Address {
 	return common.BytesToAddress(accAddress)
 }
 
+// `EthAddressToAccAddress` converts an Ethereum `Address` to a Cosmos SDK `AccAddress`.
 func EthAddressToAccAddress(ethAddress Address) sdk.AccAddress {
 	return ethAddress.Bytes()
 }
