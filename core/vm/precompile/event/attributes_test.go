f27199b9c15bfa3948b0b32ee87ddccb0f85b8f5
Cal Bera <calbera@berachain.com>
2023-01-12 15:42:45 -0800

    search func test

--- core/vm/precompile/event/attributes_test.go
+++ core/vm/precompile/event/attributes_test.go
@@ -12,19 +12,20 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package event_test
+package event
 
 import (
 	"math/big"
 	"strconv"
 	"testing"
 
-	"github.com/berachain/stargazer/common"
-	"github.com/berachain/stargazer/core/vm/precompile/event"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/berachain/stargazer/common"
 )
 
 func TestAttributes(t *testing.T) {
@@ -39,7 +40,7 @@ var _ = Describe("Attributes Test Suite", func() {
 	Describe("Test Default Attribute Value Decoder Functions", func() {
 		It("should correctly convert sdk coin strings to big.Int", func() {
 			denom10 := sdk.NewCoin("denom", sdk.NewInt(10))
-			gethValue, err = event.ConvertSdkCoin(denom10.String())
+			gethValue, err = ConvertSdkCoin(denom10.String())
 			Expect(err).To(BeNil())
 			bigVal, ok := gethValue.(*big.Int)
 			Expect(ok).To(BeTrue())
@@ -48,7 +49,7 @@ var _ = Describe("Attributes Test Suite", func() {
 
 		It("should correctly convert creation height to int64", func() {
 			creationHeightStr := strconv.FormatInt(55, 10)
-			gethValue, err = event.ConvertInt64(creationHeightStr)
+			gethValue, err = ConvertInt64(creationHeightStr)
 			Expect(err).To(BeNil())
 			int64Val, ok := gethValue.(int64)
 			Expect(ok).To(BeTrue())
@@ -57,7 +58,7 @@ var _ = Describe("Attributes Test Suite", func() {
 
 		It("should correctly convert ValAddress to common.Address", func() {
 			valAddr := sdk.ValAddress([]byte("alice"))
-			gethValue, err = event.ConvertValAddressFromBech32(valAddr.String())
+			gethValue, err = ConvertValAddressFromBech32(valAddr.String())
 			Expect(err).To(BeNil())
 			valAddrVal, ok := gethValue.(common.Address)
 			Expect(ok).To(BeTrue())
@@ -66,11 +67,32 @@ var _ = Describe("Attributes Test Suite", func() {
 
 		It("should correctly convert AccAddress to common.Address", func() {
 			accAddr := sdk.AccAddress([]byte("alice"))
-			gethValue, err = event.ConvertAccAddressFromBech32(accAddr.String())
+			gethValue, err = ConvertAccAddressFromBech32(accAddr.String())
 			Expect(err).To(BeNil())
 			accAddrVal, ok := gethValue.(common.Address)
 			Expect(ok).To(BeTrue())
 			Expect(accAddrVal).To(Equal(common.AccAddressToEthAddress(accAddr)))
+		})
+	})
+
+	Describe("Test Search Attributes for Argument", func() {
+		var attributes = []abci.EventAttribute{
+			abci.EventAttribute{Key: "k0"},
+			abci.EventAttribute{Key: "k1"},
+			abci.EventAttribute{Key: "k2"},
+			abci.EventAttribute{Key: "k3"},
+			abci.EventAttribute{Key: "k4"},
+		}
+
+		It("should return the correct index if it contains the argument name", func() {
+			Expect(searchAttributesForArg(&attributes, "k0")).To(Equal(0))
+			Expect(searchAttributesForArg(&attributes, "k3")).To(Equal(3))
+			Expect(searchAttributesForArg(&attributes, "k4")).To(Equal(4))
+		})
+
+		It("should return -1 if it does not contain the argument name", func() {
+			Expect(searchAttributesForArg(&attributes, "")).To(Equal(-1))
+			Expect(searchAttributesForArg(&attributes, "k6")).To(Equal(-1))
 		})
 	})
 })
