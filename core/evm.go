f2318371437424eb96049b77d02a8ee343a584e7
Dev Bear <itsdevbear@berachain.com>
2023-01-12 12:45:50 -0500

    name

--- core/evm.go
+++ core/evm.go
@@ -28,12 +28,12 @@ var _ vm.TransferFunc = Transfer
 
 // `CanTransfer` checks whether there are enough funds in the address' account to make a transfer.
 // NOTE: This does not take the necessary gas in to account to make the transfer valid.
-func CanTransfer(db vm.StateDB, addr common.Address, amount *big.Int) bool {
-	return db.GetBalance(addr).Cmp(amount) >= 0
+func CanTransfer(sdb vm.StateDB, addr common.Address, amount *big.Int) bool {
+	return sdb.GetBalance(addr).Cmp(amount) >= 0
 }
 
 // `Transfer` subtracts amount from sender and adds amount to recipient using the `vm.StateDB`.
-func Transfer(db vm.StateDB, sender, recipient common.Address, amount *big.Int) {
+func Transfer(sdb vm.StateDB, sender, recipient common.Address, amount *big.Int) {
 	// We use `TransferBalance` to use the same logic as the native transfer in x/bank.
-	db.(state.ExtStateDB).TransferBalance(sender, recipient, amount)
+	sdb.(state.ExtStateDB).TransferBalance(sender, recipient, amount)
 }
