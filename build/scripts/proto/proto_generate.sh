83e6a06be2c05b2c5af7d664a963ec8a20189ecc
Dev Bear <itsdevbear@berachain.com>
2023-01-13 13:42:34 -0500

    sig keys

--- build/scripts/proto/proto_generate.sh
+++ build/scripts/proto/proto_generate.sh
@@ -41,6 +41,27 @@ rm -rf github.com
 
 cd ../
 
+# for lib (todo move)
+
+cd lib/proto
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
+    # gogo proto files SHOULD ONLY be generated if this is false
+    # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
+    echo $file
+    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$'; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
+
+# move proto files to the right places
+cp -r github.com/berachain/stargazer/lib/* ../
+rm -rf github.com
+
+cd ../../
 # go mod tidy
 
 # ./build/scripts/proto/pulsar_generate.sh
---
+++ lib/crypto/secp256k1.go
@@ -0,0 +1,142 @@
+package crypto
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/subtle"
+
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	tmcrypto "github.com/tendermint/tendermint/crypto"
+)
+
+const (
+	// `PrivKeyNumBytes` defines the length of the PrivKey byte array.
+	PrivKeyNumBytes = 32
+	// `PubKeyNumBytes` defines the length of the PubKey byte array.
+	PubKeyNumBytes = 33
+	// `KeyType` is the string constant for the secp256k1 algorithm.
+	KeyType = "eth_secp256k1"
+)
+
+// =====================================================================================================
+// Public Key
+// ====================================================================================================
+
+// `Pubkey` is a wrapper around the Ethereum secp256k1 public key type. This wrapper conforms to
+// `crypotypes.Pubkey` to allow for the use of the Ethereum secp256k1 public key type within the Cosmos SDK.
+
+var (
+	_ cryptotypes.PubKey = &PubKey{}
+)
+
+// `Address` returns the address of the ECDSA public key.
+// The function will return an empty address if the public key is invalid.
+func (pubKey PubKey) Address() tmcrypto.Address {
+	pubk, err := DecompressPubkey(pubKey.Key)
+	if err != nil {
+		return nil
+	}
+
+	return tmcrypto.Address(PubkeyToAddress(*pubk).Bytes())
+}
+
+// `Bytes` returns the raw bytes of the ECDSA public key.
+func (pubKey PubKey) Bytes() []byte {
+	bz := make([]byte, len(pubKey.Key))
+	copy(bz, pubKey.Key)
+
+	return bz
+}
+
+// `Type` returns eth_secp256k1.
+func (pubKey PubKey) Type() string {
+	return KeyType
+}
+
+// `Equals` returns true if the pubkey type is the same and their bytes are deeply equal.
+func (pubKey PubKey) Equals(other cryptotypes.PubKey) bool {
+	return pubKey.Type() == other.Type() && bytes.Equal(pubKey.Bytes(), other.Bytes())
+}
+
+// `VerifySignature` verifies that the ECDSA public key created a given signature over
+// the provided message. The signature should be in [R || S] format.
+func (pubKey PubKey) VerifySignature(msg, sig []byte) bool {
+	if len(sig) == SignatureLength {
+		// remove recovery ID (V) if contained in the signature
+		sig = sig[:len(sig)-1]
+	}
+
+	// The signature needs to be in [R || S] format when provided to VerifySignature.
+	return VerifySignature(pubKey.Key, Keccak256Hash(msg).Bytes(), sig)
+}
+
+// =====================================================================================================
+// Private Key
+// ====================================================================================================
+
+// `PrivKey` is a wrapper around the Ethereum secp256k1 private key type. This wrapper conforms to
+// `crypotypes.Pubkey` to allow for the use of the Ethereum secp256k1 private key type within the Cosmos SDK.
+
+var (
+	_ cryptotypes.PrivKey = &PrivKey{}
+)
+
+// `GenerateKey` generates a new random private key. It returns an error upon
+// failure.
+func GenerateKey() (*PrivKey, error) {
+	priv, err := GenerateEthKey()
+	if err != nil {
+		return nil, err
+	}
+
+	return &PrivKey{
+		Key: FromECDSA(priv),
+	}, nil
+}
+
+// `Bytes` returns the byte representation of the ECDSA Private Key.
+func (privKey PrivKey) Bytes() []byte {
+	bz := make([]byte, len(privKey.Key))
+	copy(bz, privKey.Key)
+	return bz
+}
+
+// `PubKey` returns the ECDSA private key's public key. If the privkey is not valid
+// it returns a nil value.
+func (privKey PrivKey) PubKey() cryptotypes.PubKey {
+	ecdsaPrivKey, err := privKey.ToECDSA()
+	if err != nil {
+		return nil
+	}
+
+	return &PubKey{
+		Key: CompressPubkey(&ecdsaPrivKey.PublicKey),
+	}
+}
+
+// `s` returns true if two ECDSA private keys are equal and false otherwise.
+func (privKey PrivKey) Equals(other cryptotypes.LedgerPrivKey) bool {
+	return privKey.Type() == other.Type() && subtle.ConstantTimeCompare(privKey.Bytes(), other.Bytes()) == 1
+}
+
+// `Type` returns eth_secp256k1.
+func (privKey PrivKey) Type() string {
+	return KeyType
+}
+
+// `Sign` creates a recoverable ECDSA signature on the secp256k1 curve over the
+// provided hash of the message. The produced signature is 65 bytes
+// where the last byte contains the recovery ID.
+func (privKey PrivKey) Sign(digestBz []byte) ([]byte, error) {
+	key, err := privKey.ToECDSA()
+	if err != nil {
+		return nil, err
+	}
+
+	return EthSign(digestBz, key)
+}
+
+// `ToECDSA` returns the ECDSA private key as a reference to ecdsa.PrivateKey type.
+func (privKey PrivKey) ToECDSA() (*ecdsa.PrivateKey, error) {
+	return ToECDSA(privKey.Bytes())
+}
