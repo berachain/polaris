b7259d886919a9c8aaf0aabc66cc149f2d2914ba
Dev Bear <itsdevbear@berachain.com>
2023-01-14 21:28:48 -0500

    bing bong

--- core/vm/precompile/event/attributes_test.go
+++ core/vm/precompile/event/attributes_test.go
@@ -24,7 +24,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 
-	"github.com/berachain/stargazer/common"
+	"github.com/berachain/stargazer/lib/common"
 )
 
 var _ = Describe("Attributes Test Suite", func() {
