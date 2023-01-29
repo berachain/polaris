a27195ac9d4b6a1dd1205baa96d336aae2081c0c
Devon Bear <itsdevbear@berachain.com>
2023-01-28 22:09:31 -0500

    snapshottable-store

---
+++ store/cachekv/cache_entry_test.go
@@ -0,0 +1,172 @@
+// Copyright (C) 2022, Berachain Foundation. All rights reserved.
+// See the file LICENSE for licensing terms.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package cachekv
+
+import (
+	"reflect"
+	"testing"
+
+	sdkcachekv "github.com/cosmos/cosmos-sdk/store/cachekv"
+	"github.com/cosmos/cosmos-sdk/store/dbadapter"
+	"github.com/stretchr/testify/suite"
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/berachain/stargazer/lib/utils"
+	"github.com/berachain/stargazer/x/evm/plugins/state/store/journal"
+)
+
+var (
+	byte0    = []byte{0}
+	byte1    = []byte{1}
+	byte0Str = utils.UnsafeBytesToStr(byte0)
+	byte1Str = utils.UnsafeBytesToStr(byte1)
+)
+
+type CacheValueSuite struct {
+	suite.Suite
+	cacheKVStore *Store
+}
+
+func TestCacheValueSuite(t *testing.T) {
+	suite.Run(t, new(CacheValueSuite))
+}
+
+func (s *CacheValueSuite) SetupTest() {
+	parent := sdkcachekv.NewStore(dbadapter.Store{DB: dbm.NewMemDB()})
+	parent.Set(byte0, byte0)
+	s.cacheKVStore = NewStore(parent, journal.NewManager())
+}
+
+func (s *CacheValueSuite) TestRevertDeleteAfterNothing() {
+	// delete after nothing happened to key
+	snapshot := s.cacheKVStore.Snapshot()
+	// delete key: 0
+	s.cacheKVStore.Delete(byte0)
+	s.Require().Equal(([]byte)(nil), s.cacheKVStore.Cache[byte0Str].value)
+	s.Require().True(s.cacheKVStore.Cache[byte0Str].dirty)
+	s.Require().Contains(s.cacheKVStore.UnsortedCache, byte0Str)
+	// revert delete key: 0
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().NotContains(s.cacheKVStore.Cache, byte0Str)
+	s.Require().NotContains(s.cacheKVStore.UnsortedCache, byte0Str)
+}
+
+func (s *CacheValueSuite) TestRevertDeleteAfterGet() {
+	// delete after Get called on key
+	_ = s.cacheKVStore.Get(byte0)
+	snapshot := s.cacheKVStore.Snapshot()
+	// delete key: 0
+	s.cacheKVStore.Delete(byte0)
+	// revert delete key: 0
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().Equal(byte0, s.cacheKVStore.Cache[byte0Str].value)
+	s.Require().False(s.cacheKVStore.Cache[byte0Str].dirty)
+	s.Require().NotContains(s.cacheKVStore.UnsortedCache, byte0Str)
+}
+
+func (s *CacheValueSuite) TestRevertDeleteAfterSet() {
+	// delete after Set called on key
+	s.cacheKVStore.Set(byte1, byte1)
+	snapshot := s.cacheKVStore.Snapshot()
+	// delete key: 1
+	s.cacheKVStore.Delete(byte1)
+	// revert delete key: 1
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().Equal(byte1, s.cacheKVStore.Cache[byte1Str].value)
+	s.Require().True(s.cacheKVStore.Cache[byte1Str].dirty)
+	s.Require().Contains(s.cacheKVStore.UnsortedCache, byte1Str)
+}
+
+func (s *CacheValueSuite) TestRevertDeleteAfterDelete() {
+	// delete after Delete called on key
+	s.cacheKVStore.Delete(byte0)
+	snapshot := s.cacheKVStore.Snapshot()
+	// delete key: 0
+	s.cacheKVStore.Delete(byte0)
+	// revert delete key: 0
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().Equal(([]byte)(nil), s.cacheKVStore.Cache[byte0Str].value)
+	s.Require().True(s.cacheKVStore.Cache[byte0Str].dirty)
+	s.Require().Contains(s.cacheKVStore.UnsortedCache, byte0Str)
+}
+
+func (s *CacheValueSuite) TestRevertSetAfterNothing() {
+	// set after nothing happened to key
+	snapshot := s.cacheKVStore.Snapshot()
+	// set key: 1
+	s.cacheKVStore.Set(byte1, byte1)
+	s.Require().Equal(byte1, s.cacheKVStore.Cache[byte1Str].value)
+	s.Require().True(s.cacheKVStore.Cache[byte1Str].dirty)
+	s.Require().Contains(s.cacheKVStore.UnsortedCache, byte1Str)
+	// revert set key: 1
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().NotContains(s.cacheKVStore.Cache, byte1Str)
+	s.Require().NotContains(s.cacheKVStore.UnsortedCache, byte1Str)
+}
+
+func (s *CacheValueSuite) TestRevertSetAfterGet() {
+	// set after get called on key
+	_ = s.cacheKVStore.Get(byte0)
+	snapshot := s.cacheKVStore.Snapshot()
+	// set key: 0 to val: 1
+	s.cacheKVStore.Set(byte0, byte1)
+	// revert set key: 1 to val: 1
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().Equal(byte0, s.cacheKVStore.Cache[byte0Str].value)
+	s.Require().False(s.cacheKVStore.Cache[byte0Str].dirty)
+	s.Require().NotContains(s.cacheKVStore.UnsortedCache, byte0Str)
+}
+
+func (s *CacheValueSuite) TestRevertSetAfterDelete() {
+	// set after delete called on key
+	s.cacheKVStore.Delete(byte0)
+	snapshot := s.cacheKVStore.Snapshot()
+	// set key: 0 to val: 0
+	s.cacheKVStore.Set(byte0, byte0)
+	// revert set key: 0 to val: 0
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().Nil(s.cacheKVStore.Cache[byte0Str].value)
+	s.Require().True(s.cacheKVStore.Cache[byte0Str].dirty)
+	s.Require().Contains(s.cacheKVStore.UnsortedCache, byte0Str)
+}
+
+func (s *CacheValueSuite) TestRevertSetAfterSet() {
+	// set after set called on key
+	s.cacheKVStore.Set(byte1, byte1)
+	snapshot := s.cacheKVStore.Snapshot()
+	// set key: 1 to val: 0
+	s.cacheKVStore.Set(byte1, byte0)
+	// revert set key: 1 to val: 0
+	s.cacheKVStore.RevertToSnapshot(snapshot)
+	s.Require().Equal(byte1, s.cacheKVStore.Cache[byte1Str].value)
+	s.Require().True(s.cacheKVStore.Cache[byte1Str].dirty)
+	s.Require().Contains(s.cacheKVStore.UnsortedCache, byte1Str)
+}
+
+func (s *CacheValueSuite) TestCloneSet() {
+	dcvNonNil := newCacheEntry(s.cacheKVStore, byte1Str, newCacheValue(byte1, true))
+	dcvNonNilClone, ok := dcvNonNil.Clone().(*cacheEntry)
+	s.Require().True(ok)
+	s.Require().Equal(byte1Str, dcvNonNilClone.Key)
+	s.Require().True(dcvNonNilClone.Prev.dirty)
+	s.Require().Equal(byte1, dcvNonNilClone.Prev.value)
+
+	dcvNil := newCacheEntry(s.cacheKVStore, "", nil)
+	dcvNilClone, ok := dcvNil.Clone().(*cacheEntry)
+	s.Require().True(ok)
+	s.Require().Equal("", dcvNilClone.Key)
+	s.Require().Equal(dcvNil.Prev, dcvNilClone.Prev)
+	s.Require().True(reflect.ValueOf(dcvNilClone.Prev).IsNil())
+}
---
+++ store/cachekv/cache_value.go
@@ -0,0 +1,41 @@
+// Copyright (C) 2023, Berachain Foundation. All rights reserved.
+// See the file LICENSE for licensing terms.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package cachekv
+
+import libtypes "github.com/berachain/stargazer/lib/types"
+
+// Compile-time assertion that `cacheValue` implements `types.Cloneable`.
+var _ libtypes.Cloneable[*cacheValue] = (*cacheValue)(nil)
+
+// `cacheValue` represents a cached value in the cachekv store.
+// If dirty is true, it indicates the cached value is different from the underlying value.
+type cacheValue struct {
+	value []byte
+	dirty bool
+}
+
+// `newCacheValue` creates a new `cacheValue` object with the given `value` and `dirty` flag.
+func newCacheValue(v []byte, d bool) *cacheValue {
+	return &cacheValue{
+		value: v,
+		dirty: d,
+	}
+}
+
+// `Clone` implements `types.Cloneable`.
+func (cv *cacheValue) Clone() *cacheValue {
+	// Return a new cacheValue with the same value and dirty flag
+	return newCacheValue(append([]byte(nil), cv.value...), cv.dirty)
+}
