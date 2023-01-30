758dd67fbc0edb63185c0a660bf3cfcda9029d9a
Devon Bear <itsdevbear@berachain.com>
2023-01-30 12:17:51 -0500

    feat(lib): Cloneable Stack (#98)

--- lib/ds/ds.go
+++ lib/ds/ds.go
@@ -14,7 +14,10 @@
 
 package ds
 
-import dbm "github.com/tendermint/tm-db"
+import (
+	libtypes "github.com/berachain/stargazer/lib/types"
+	dbm "github.com/tendermint/tm-db"
+)
 
 // `Stack` is an interface that defines the methods that an items Stack must implement.
 // items Stacks support holding cache entries and reverting to a certain index.
@@ -37,6 +40,19 @@ type Stack[Item any] interface {
 
 	// `Size` returns the current number of entries in the items.
 	Size() int
+
+	// `Capacity` returns the size of the allocated buffer for the stack.
+	Capacity() int
+}
+
+// `CloneableStack` is an interface that extends `Stack` to allow for deep copying.
+// As such, the items in the stack must implement `Cloneable`.
+type CloneableStack[T libtypes.Cloneable[T]] interface {
+	// `CloneableStack` is a `Stack`.
+	Stack[T]
+
+	// `CloneableStack` implements `Cloneable`.
+	libtypes.Cloneable[CloneableStack[T]]
 }
 
 // `BTree` is an interface that defines the methods a binary tree must implement.
