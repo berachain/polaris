805da31e0ed9f4a62a58f9e1429535cdbe4bf86a
Devon Bear <itsdevbear@berachain.com>
2023-02-01 22:08:50 -0500

    improve stack coverage

--- lib/ds/stack/stack_test.go
+++ lib/ds/stack/stack_test.go
@@ -40,22 +40,18 @@ var _ = Describe("Stack", func() {
 		BeforeEach(func() {
 			s.Push(1)
 		})
-
 		It("should not be empty", func() {
 			Expect(s.Size()).To(Equal(1))
 		})
-
 		It("should return the correct element", func() {
 			Expect(s.Peek()).To(Equal(1))
 		})
-
 		It("should return the correct element", func() {
 			Expect(s.PeekAt(0)).To(Equal(1))
 		})
 		It("should return the correct element", func() {
 			Expect(s.Pop()).To(Equal(1))
 		})
-
 		When("popping an element", func() {
 			BeforeEach(func() {
 				s.Pop()
@@ -65,7 +61,6 @@ var _ = Describe("Stack", func() {
 				Expect(s.Size()).To(BeZero())
 			})
 		})
-
 		When("pushing more elements", func() {
 			BeforeEach(func() {
 				s.Push(2)
@@ -95,6 +90,52 @@ var _ = Describe("Stack", func() {
 					Expect(s.Peek()).To(Equal(1))
 					Expect(s.PeekAt(0)).To(Equal(1))
 				})
+			})
+		})
+		When("PopToSize is called with a size larger than the current size", func() {
+			It("should panic", func() {
+				Expect(func() {
+					s.PopToSize(2)
+				}).To(Panic())
+			})
+		})
+		When("pop to size zero is called", func() {
+			BeforeEach(func() {
+				s.PopToSize(0)
+			})
+
+			It("should be empty", func() {
+				Expect(s.Size()).To(BeZero())
+			})
+		})
+		When("calling pop on an empty stack", func() {
+			It("should return an empty stack", func() {
+				s.Pop()
+				Expect(s.Size()).To(BeZero())
+			})
+
+			It("should return a nil element", func() {
+				Expect(s.Pop()).To(Equal(1))
+				Expect(s.Pop()).To(BeZero())
+			})
+		})
+		When("calling peek on an empty stack", func() {
+			It("should return an empty stack", func() {
+				s.Pop()
+				Expect(s.Peek()).To(BeZero())
+				Expect(s.Size()).To(BeZero())
+			})
+
+			It("should return a nil element", func() {
+				s.PopToSize(0)
+				Expect(s.Peek()).To(BeZero())
+			})
+		})
+		When("calling peekat with an index too large", func() {
+			It("should panic", func() {
+				Expect(func() {
+					s.PeekAt(10)
+				}).To(Panic())
 			})
 		})
 	})
