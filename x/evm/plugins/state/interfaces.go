ce960844d33d5ae51fa2f2376f5ab5ef93b23c03
Devon Bear <itsdevbear@berachain.com>
2023-02-02 15:23:34 -0500

    refator(statetypes): Types -> Storage (#119)

--- x/evm/plugins/state/interfaces.go
+++ x/evm/plugins/state/interfaces.go
@@ -12,7 +12,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package types
+package state
 
 import (
 	coretypes "github.com/berachain/stargazer/eth/core/types"
