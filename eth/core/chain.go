e0a9ff0dcedd803771d0e5b34851f2059a3a8083
Devon Bear <itsdevbear@berachain.com>
2023-02-22 14:13:51 -0500

    add caches

--- eth/core/chain.go
+++ eth/core/chain.go
@@ -42,6 +42,7 @@ type blockchain struct {
 	// `host` is the host chain that the Stargazer EVM is running on.
 	host StargazerHostChain
 
+	// `finalizedBlock` is the last finalized block.
 	finalizedBlock atomic.Value
 
 	// `receiptsCache` is a cache of the receipts for the last `defaultCacheSizeBytes` bytes of blocks.
@@ -88,8 +89,16 @@ func (bc *blockchain) Prepare(ctx context.Context, height int64) {
 	// If we are processing a new block, then we assume that the previous was finalized.
 	// TODO: ensure this is safe. We could build the block in theory by querying the blockplugin
 	if bc.processor.block != nil {
+		// Cache finalized block.
 		bc.finalizedBlock.Store(bc.processor.block)
 		bc.blockCache.Add(bc.processor.block.Hash(), bc.processor.block)
+
+		// Cache transaction data
+		for _, tx := range bc.processor.block.GetTransactions() {
+			bc.txLookupCache.Add(tx.Hash(), tx)
+		}
+		// Cache receipts.
+		bc.receiptsCache.Add(bc.processor.block.Hash(), bc.processor.block.GetReceipts())
 	}
 	bc.processor.Prepare(ctx, height)
 }
