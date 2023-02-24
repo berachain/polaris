ba82ce923d951cc5ec70c259162c299c351fc352
Devon Bear <itsdevbear@berachain.com>
2023-02-24 14:50:25 -0500

    register with module

--- x/evm/module.go
+++ x/evm/module.go
@@ -124,7 +124,7 @@ func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 // `RegisterServices` registers a gRPC query service to respond to the
 // module-specific gRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	// types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
+	types.RegisterMsgServiceServer(cfg.MsgServer(), am.keeper)
 	// types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
 
