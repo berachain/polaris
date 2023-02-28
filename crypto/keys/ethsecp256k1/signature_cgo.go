00f117a1da7299a16a1dbc412bba3fd15e2eacfb
Devon Bear <itsdevbear@berachain.com>
2023-02-27 20:33:39 -0500

    gang

--- crypto/keys/ethsecp256k1/signature_cgo.go
+++ crypto/keys/ethsecp256k1/signature_cgo.go
@@ -46,6 +46,10 @@ func (privKey PrivKey) Sign(digestBz []byte) ([]byte, error) {
 // the provided message. The signature should be in [R || S] format.
 func (pubKey PubKey) VerifySignature(msg, sig []byte) bool {
 	fmt.Println("HELLO WELCOME TO THE PARTY", msg, sig)
+
+	if len(msg) != crypto.DigestLength {
+		msg = crypto.Keccak256(msg)
+	}
 	if len(sig) == crypto.SignatureLength {
 		// remove recovery ID (V) if contained in the signature
 		sig = sig[:len(sig)-1]
@@ -53,5 +57,5 @@ func (pubKey PubKey) VerifySignature(msg, sig []byte) bool {
 	}
 
 	// The signature needs to be in [R || S] format when provided to `VerifySignature`.
-	return crypto.VerifySignature(pubKey.Key, crypto.Keccak256(msg), sig)
+	return crypto.VerifySignature(pubKey.Key, msg, sig)
 }
