1c36f7d7837b695a1a00b581ddb505c4e5a54088
t12s <t12s@berachain.com>
2023-03-01 17:45:33 -0500

    added more network tests

--- testutil/network/network_test.go
+++ testutil/network/network_test.go
@@ -22,15 +22,37 @@ package network_test
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"math/big"
 	"testing"
 	"time"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"pkg.berachain.dev/stargazer/eth/common"
+	"pkg.berachain.dev/stargazer/eth/params"
+
 	"pkg.berachain.dev/stargazer/testutil/network"
+)
+
+var (
+	dummyContract = common.HexToAddress("0x9fd0aA3B78277a1E717de9D3de434D4b812e5499")
+	testKey, _    = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+
+	signer = types.LatestSignerForChainID(params.DefaultChainConfig.ChainID)
+
+	txData = &types.DynamicFeeTx{
+		Nonce: 0,
+		To:    &dummyContract,
+		Gas:   100000,
+		Data:  []byte("abcdef"),
+	}
 )
 
 func TestNetwork(t *testing.T) {
@@ -38,54 +60,143 @@ func TestNetwork(t *testing.T) {
 	RunSpecs(t, "testutil/network:integration")
 }
 
-var _ = Describe("NetworkRPC", func() {
+var _ = Describe("BlockAPIs", func() {
 	var net *network.Network
+	var client *ethclient.Client
+
 	BeforeEach(func() {
-		net = network.New(GinkgoT(), network.DefaultConfig())
-		_, err := net.WaitForHeightWithTimeout(3, 15*time.Second)
+		cfg := network.DefaultConfig()
+		genesis := make(map[string]json.RawMessage)
+		fmt.Println("genesis", genesis)
+
+		net = network.New(GinkgoT(), cfg)
+		_, err := net.WaitForHeightWithTimeout(1, 15*time.Second)
+		Expect(err).To(BeNil())
+		client, err = ethclient.Dial(net.Validators[0].APIAddress + "/eth/rpc")
 		Expect(err).To(BeNil())
 
 	})
 
-	It("eth_chainId", func() {
-		client, err := ethclient.Dial(net.Validators[0].APIAddress + "/eth/rpc")
-		Expect(err).To(BeNil())
+	It("eth_chainId - get chain id", func() {
 		chainID, err := client.ChainID(context.Background())
 		Expect(err).To(BeNil())
 		Expect(chainID.String()).To(Equal("42069"))
 	})
 
-	It("eth_blockNumber", func() {
+	It("eth_blockNumber - get latest block number", func() {
 		// Dial GetBlock
-		client, err := ethclient.Dial(net.Validators[0].APIAddress + "/eth/rpc")
-		Expect(err).To(BeNil())
 		blockNumber, err := client.BlockNumber(context.Background())
 		Expect(err).To(BeNil())
 		Expect(blockNumber).To(BeNumerically(">", 0))
 	})
 
-	// TODO: get blockByNumber fails on repeated calls
-	It("eth_getBlockByNumber", func() {
-		client, err := ethclient.Dial(net.Validators[0].APIAddress + "/eth/rpc")
+	It("eth_getBlockByNumber - get block header by number", func() {
+		blockNumber, err := client.BlockNumber(context.Background())
 		Expect(err).To(BeNil())
+		header, err := client.HeaderByNumber(context.Background(), big.NewInt(int64(blockNumber)))
+		Expect(err).To(BeNil())
+		Expect(header.Number).To(Equal(blockNumber))
+	})
+
+	It("eth_getBlockByHash - get block header by hash for block 1", func() {
+
+		// TODO: expected failure because offchain kv is not working yet, this reads from offchain kv
+		block, err := client.BlockByNumber(context.Background(), big.NewInt(1))
+		Expect(err).To(BeNil())
+		hash := block.Hash()
+		blockHeaderByHash, err := client.HeaderByHash(context.Background(), hash)
+		Expect(err).To(BeNil())
+		Expect(*blockHeaderByHash).To(Equal(*block.Header()))
+		Expect(blockHeaderByHash.Number).To(Equal(block.Number()))
+	})
+
+	It("eth_getBlockTransactionCountByHash - get txns in block by block hash. No txns submitted", func() {
 		blockNumber, err := client.BlockNumber(context.Background())
 		Expect(err).To(BeNil())
 		block, err := client.BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
 		Expect(err).To(BeNil())
 		Expect(block.Number().Uint64()).To(Equal(blockNumber))
+		count, err := client.TransactionCount(context.Background(), block.Hash())
+		Expect(err).To(BeNil())
+		Expect(count).To(Equal(0))
+
 	})
 
-	// TODO: getBlockByHash fails because it's dependent on above problem
-	// It("eth_getBlockByHash", func() {
+	// TODO: get blockByNumber fails on repeated calls
+	It("eth_getBlockTransactionCountByHash - get txns in block by block hash. 1 txn submitted", func() {
 
-	// 	// TODO: expected failure because offchain kv is not working yet, this reads from offchain kv
-	// 	client, err := ethclient.Dial(net.Validators[0].APIAddress + "/eth/rpc")
-	// 	Expect(err).To(BeNil())
-	// 	block, err := client.BlockByNumber(context.Background(), big.NewInt(1))
-	// 	Expect(err).To(BeNil())
-	// 	hash := block.Hash()
-	// 	blockByHash, err := client.BlockByHash(context.Background(), hash)
-	// 	Expect(err).To(BeNil())
-	// 	Expect(blockByHash.Hash()).To(Equal(hash))
+		tx := types.NewTx(txData)
+		signedTx, err := types.SignTx(tx, signer, testKey)
+		Expect(err).To(BeNil())
+		// send transaction
+		err = client.SendTransaction(context.Background(), signedTx)
+		Expect(err).To(BeNil())
+
+	})
+
+})
+
+var _ = Describe("TransactionAPIs", func() {
+	var net *network.Network
+	var client *ethclient.Client
+	BeforeEach(func() {
+		net = network.New(GinkgoT(), network.DefaultConfig())
+		_, err := net.WaitForHeightWithTimeout(3, 15*time.Second)
+		Expect(err).To(BeNil())
+		client, err = ethclient.Dial(net.Validators[0].APIAddress + "/eth/rpc")
+		Expect(err).To(BeNil())
+
+	})
+
+	// TODO: fails currently as unable to check for signed tx
+	It("eth_sendTransaction", func() {
+		// create signed transaction
+		tx := types.NewTx(txData)
+		signedTx, err := types.SignTx(tx, signer, testKey)
+		Expect(err).To(BeNil())
+		// send transaction
+		err = client.SendTransaction(context.Background(), signedTx)
+		Expect(err).To(BeNil())
+	})
+
+	// TODO: fails currently as unable to check for signed tx
+	It("eth_getTransactionByHash", func() {
+		// create signed transaction
+		tx := types.NewTx(txData)
+		signedTx, err := types.SignTx(tx, signer, testKey)
+		Expect(err).To(BeNil())
+		// send transaction
+		err = client.SendTransaction(context.Background(), signedTx)
+		Expect(err).To(BeNil())
+
+		txReceived, isPending, err := client.TransactionByHash(context.Background(), signedTx.Hash())
+		Expect(err).To(BeNil())
+		Expect(isPending).To(BeFalse())
+		Expect(txReceived.Hash()).To(Equal(signedTx.Hash()))
+	})
+
+	It("eth_getTransactionReceipt - get txn receipt for txn submitted by hash", func() {
+		tx := types.NewTx(txData)
+		signedTx, err := types.SignTx(tx, signer, testKey)
+		Expect(err).To(BeNil())
+		err = client.SendTransaction(context.Background(), signedTx)
+		Expect(err).To(BeNil())
+		receipt, err := client.TransactionReceipt(context.Background(), signedTx.Hash())
+		Expect(err).To(BeNil())
+		Expect(receipt.TxHash).To(Equal(signedTx.Hash()))
+
+	})
+
+	// TODO: verify if this works like this
+	It("eth_getTransactionByBlockHashAndIndex - get txn by block hash and index for block 1 (no txns)", func() {
+		block, err := client.BlockByNumber(context.Background(), big.NewInt(1))
+		Expect(err).To(BeNil())
+		_, err = client.TransactionInBlock(context.Background(), block.Hash(), 1)
+		Expect(err).To(BeNil())
+	})
+
+	// // TODO: same as above so need to understand how it works exactly
+	// It("eth_getTransactionByBlockNumberAndIndex - get txn by block num and index for block 1 (1 txn)", func() {
+	// 	Expect(true)
 	// })
 })
