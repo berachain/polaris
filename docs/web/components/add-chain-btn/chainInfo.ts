07cab87effe74f7bcb2e4282ed91ed481259dde4
Garrett Parris <garrettparris@Garretts-MacBook-Pro.local>
2023-03-01 17:19:13 -0500

    edit sweep

--- docs/web/components/add-chain-btn/chainInfo.ts
+++ docs/web/components/add-chain-btn/chainInfo.ts
@@ -3,19 +3,13 @@ import { Bech32Address } from '@keplr-wallet/cosmos';
 
 const coinType60 = 60
 
-const beraBip44: BIP44 = {
+const gazerBip44: BIP44 = {
   coinType: coinType60,
 };
 
-const BERA = {
-  coinDenom: 'bera',
-  coinMinimalDenom: 'abera',
-  coinDecimals: 18,
-};
-
-const BGT = {
-  coinDenom: 'bgt',
-  coinMinimalDenom: 'abgt',
+const GAZER = {
+  coinDenom: 'gazer',
+  coinMinimalDenom: 'agazer',
   coinDecimals: 18,
 };
 
@@ -23,14 +17,14 @@ const LOCAL_CHAIN_INFO: ChainInfo = {
   coinType: coinType60,
   rpc: 'http://localhost:26657',
   rest: 'http://localhost:1317',
-  chainId: 'berachain_420-1',
-  chainName: 'berachain-devnet',
-  stakeCurrency: BGT,
-  bip44: beraBip44,
-  bech32Config: Bech32Address.defaultBech32Config('bera'),
-  currencies: [BERA, BGT],
+  chainId: 'stargazer-2061',
+  chainName: 'stargazer',
+  stakeCurrency: GAZER,
+  bip44: gazerBip44,
+  bech32Config: Bech32Address.defaultBech32Config('stargazer'),
+  currencies: [GAZER],
   feeCurrencies: [{
-    ...BERA,
+    ...GAZER,
     gasPriceStep: {
       low: 10000000000,
       average: 25000000000,
