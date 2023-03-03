b18be8738a267f6ed794ed055561f41110be158f
Never_Defined <hk-nft@protonmail.com>
2023-03-03 13:46:07 -0500

    lint

--- precompile/bank/bank_test.go
+++ precompile/bank/bank_test.go
@@ -1,17 +1,37 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2023, Berachain Foundation. All rights reserved.
+// Use of this software is govered by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 package bank
 
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
 	"pkg.berachain.dev/stargazer/eth/core/vm"
 	"pkg.berachain.dev/stargazer/lib/utils"
 	"pkg.berachain.dev/stargazer/x/evm/plugins/precompile/log"
 
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 func TestBankPrecompile(t *testing.T) {
@@ -42,7 +62,7 @@ var _ = Describe("Bank Precompile Test", func() {
 			sdk.NewAttribute(sdk.AttributeKeyAmount, sdk.NewCoin("stg", sdk.NewInt(100)).String()),
 		)
 		log, err := factory.Build(&event)
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(log.Address).To(Equal(contract.RegistryKey()))
 	})
 
@@ -53,7 +73,7 @@ var _ = Describe("Bank Precompile Test", func() {
 			sdk.NewAttribute(sdk.AttributeKeyAmount, sdk.NewCoin("stg", sdk.NewInt(100)).String()),
 		)
 		log, err := factory.Build(&event)
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(log.Address).To(Equal(contract.RegistryKey()))
 	})
 
@@ -64,7 +84,7 @@ var _ = Describe("Bank Precompile Test", func() {
 			sdk.NewAttribute(sdk.AttributeKeyAmount, sdk.NewCoin("stg", sdk.NewInt(100)).String()),
 		)
 		log, err := factory.Build(&event)
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(log.Address).To(Equal(contract.RegistryKey()))
 	})
 
@@ -75,7 +95,7 @@ var _ = Describe("Bank Precompile Test", func() {
 			sdk.NewAttribute(sdk.AttributeKeyAmount, sdk.NewCoin("stg", sdk.NewInt(100)).String()),
 		)
 		log, err := factory.Build(&event)
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(log.Address).To(Equal(contract.RegistryKey()))
 	})
 
@@ -86,7 +106,7 @@ var _ = Describe("Bank Precompile Test", func() {
 			sdk.NewAttribute(sdk.AttributeKeyAmount, sdk.NewCoin("stg", sdk.NewInt(100)).String()),
 		)
 		log, err := factory.Build(&event)
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(log.Address).To(Equal(contract.RegistryKey()))
 	})
 })
