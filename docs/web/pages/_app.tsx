d493828921c20b08d16f4a0c0dbff0ed2db01536
Garrett Parris <garrettparris@Garretts-MacBook-Pro.local>
2023-03-05 14:44:54 -0500

    updated landing page

--- docs/web/pages/_app.tsx
+++ docs/web/pages/_app.tsx
@@ -1,12 +1,18 @@
-import type { ReactElement } from 'react'
-import type { AppProps } from 'next/app'
+import './styles.css'
+import 'nextra-theme-docs/style.css'
+import Head from 'next/head'
 
-import '../style.css'
-
-export default function Nextra({
-  Component,
-  pageProps
-}: AppProps): ReactElement {
-  
-  return <Component {...pageProps} />
+export default function Nextra({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page)
+  return getLayout(
+    <>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans:wght@400;700&display=block'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- docs/web/theme.config.jsx
+++ docs/web/theme.config.jsx
@@ -84,5 +84,6 @@ export default {
                 </p>
             </div>
         )
-    }
+    },
+    gitTimestamp: false,
 }
