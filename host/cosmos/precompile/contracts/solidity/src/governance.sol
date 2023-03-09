d5552317413b0131a45f9df8e246630a126f8b9b
Never_Defined <hk-nft@protonmail.com>
2023-03-09 16:42:56 -0500

    cleanup-comments

--- host/cosmos/precompile/contracts/solidity/src/governance.sol
+++ host/cosmos/precompile/contracts/solidity/src/governance.sol
@@ -1,9 +1,29 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (C) 2023, Berachain Foundation. All rights reserved.
+// Use of this software is govered by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
 pragma solidity ^0.8.4;
 
 interface IGovernanceModule {
     ////////////////////////////////////////// Write Methods /////////////////////////////////////////////
     /**
-     *@dev Submit a proposal to the governance module. Returns the proposal id.
+     * @dev Submit a proposal to the governance module. Returns the proposal id.
      */
     function submitProposal(
         bytes calldata message,
@@ -15,7 +35,7 @@ interface IGovernanceModule {
     ) external returns (uint64);
 
     /**
-     *@dev Cancel a proposal. Returns the cancled time and height.
+     * @dev Cancel a proposal. Returns the cancled time and height.
       burned.
      */
     function cancelProposal(
@@ -32,7 +52,7 @@ interface IGovernanceModule {
     ) external;
 
     /**
-     *@dev Vote on a proposal with weights.
+     * @dev Vote on a proposal with weights.
      */
     function voteWeighted(
         uint64 proposalId,
@@ -43,7 +63,7 @@ interface IGovernanceModule {
     ////////////////////////////////////////// Read Methods /////////////////////////////////////////////
 
     /**
-     *@dev Get the proposal with the given id.
+     * @dev Get the proposal with the given id.
      */
     function getProposal(
         uint256 proposalId
