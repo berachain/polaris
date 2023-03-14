83f738f3f3238269af7d6d5058cff0a663bc63aa
Cal Bera <calbera@berachain.com>
2023-03-14 11:19:52 -0400

    use atomic Pointer

--- eth/core/chain.go
+++ eth/core/chain.go
@@ -62,9 +62,9 @@ type blockchain struct {
 	vmConfig *vm.Config
 
 	// `currentBlock` is the current/pending block.
-	currentBlock atomic.Value
+	currentBlock atomic.Pointer[types.Block]
 	// `finalizedBlock` is the finalized/latest block.
-	finalizedBlock atomic.Value
+	finalizedBlock atomic.Pointer[types.Block]
 	// `currentReceipts` is the current/pending receipts.
 	currentReceipts atomic.Value
 	// `currentLogs` is the current/pending logs.
