95a8b3ebada256835ba1a71d9aa010f153af6d79
Cal Bera <calbera@berachain.com>
2023-04-04 11:32:57 -0400

    lint

--- eth/core/processor.go
+++ eth/core/processor.go
@@ -26,6 +26,7 @@ import (
 	"sync"
 
 	"github.com/ethereum/go-ethereum/trie"
+
 	"pkg.berachain.dev/polaris/eth/core/precompile"
 	"pkg.berachain.dev/polaris/eth/core/types"
 	"pkg.berachain.dev/polaris/eth/core/vm"
