467c34237e366be3db39ad1c7f099ce4b3a86bd3
Cal Bera <calbera@berachain.com>
2023-04-17 14:50:06 -0400

    more check

--- cosmos/testing/integration/precompile/misc/precompile_constructor_test.go
+++ cosmos/testing/integration/precompile/misc/precompile_constructor_test.go
@@ -38,6 +38,16 @@ var _ = Describe("Miscellaneous Precompile Tests", func() {
 			ExpectSuccessReceipt(tf.EthClient, tx)
 			Expect(contract).ToNot(BeNil())
 			Expect(addr).ToNot(BeEmpty())
+
+			err = tf.Network.WaitForNextBlock()
+			Expect(err).NotTo(HaveOccurred())
+
+			aberaAddr, err := contract.Abera(nil)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(aberaAddr).ToNot(BeEmpty())
+			aberaStr, err := contract.Denom(nil)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(aberaStr).To(Equal("abera"))
 		})
 	})
 })
