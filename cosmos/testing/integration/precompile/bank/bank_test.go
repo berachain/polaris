91ab2b3ae5d3d0aca3ac02539a4629877bc8e005
Cal Bera <calbera@berachain.com>
2023-05-08 15:46:56 -0400

    fix tests

--- cosmos/testing/integration/precompile/bank/bank_test.go
+++ cosmos/testing/integration/precompile/bank/bank_test.go
@@ -18,7 +18,7 @@
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
 // TITLE.
 
-package bank
+package bank_test
 
 import (
 	"math/big"
--- cosmos/testing/integration/precompile/erc20/erc20_test.go
+++ cosmos/testing/integration/precompile/erc20/erc20_test.go
@@ -18,7 +18,7 @@
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
 // TITLE.
 
-package erc20
+package erc20_test
 
 import (
 	"math/big"
