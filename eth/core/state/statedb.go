27be42139a7bb92c3f482d78952916ca48e66bda
Devon Bear <itsdevbear@berachain.com>
2023-05-18 20:15:27 -0400

    fix

--- eth/core/state/statedb.go
+++ eth/core/state/statedb.go
@@ -96,7 +96,6 @@ func (sdb *stateDB) RevertToSnapshot(id int) {
 // Reset sets the TxContext for the current transaction, blocking until finalize is called for the
 // previous transaction.
 func (sdb *stateDB) Reset(txHash common.Hash, txIndex int) {
-	sdb.mtx.Lock()
 	sdb.LogsJournal.SetTxContext(txHash, txIndex)
 }
 
