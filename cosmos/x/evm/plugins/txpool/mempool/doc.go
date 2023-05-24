0dfac3240be9a01a1c9591acbc8b3889763aa538
Devon Bear <itsdevbear@berachain.com>
2023-05-24 13:58:34 -0400

    docs

---
+++ cosmos/x/evm/plugins/txpool/mempool/doc.go
@@ -0,0 +1,6 @@
+/*
+The mempool package provides a wrapper around a standard Cosmos SDK's app-side mempool implementation.
+This allows for the management of Ethereum transactions and provides visibility for the `PolarisBackend` and thus
+by extension enables the Ethereum JSON-RPC server's `txpool` namespace to function correctly.
+*/
+package mempool
