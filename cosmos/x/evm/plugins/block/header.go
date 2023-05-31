27cb8e5652cb5d2dd61482ed5d586f7ee9791edd
Devon Bear <itsdevbear@berachain.com>
2023-05-30 22:33:55 -0400

    more bing bong

--- cosmos/x/evm/plugins/block/header.go
+++ cosmos/x/evm/plugins/block/header.go
@@ -22,13 +22,11 @@ package block
 
 import (
 	"errors"
-	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"pkg.berachain.dev/polaris/cosmos/x/evm/types"
 	coretypes "pkg.berachain.dev/polaris/eth/core/types"
-	"pkg.berachain.dev/polaris/eth/rpc"
 	errorslib "pkg.berachain.dev/polaris/lib/errors"
 )
 
@@ -49,16 +47,12 @@ func (p *plugin) GetHeaderByNumber(number uint64) (*coretypes.Header, error) {
 		return nil, errors.New("GetHeader: getQueryContext is nil")
 	}
 
+	// TODO: ensure we aren't differing from geth / hiding errors here.
 	if number > uint64(p.ctx.BlockHeight()) {
 		number = uint64(p.ctx.BlockHeight())
 	}
 
-	iavlHeight, err := p.getIAVLHeight(int64(number))
-	if err != nil {
-		return nil, errorslib.Wrapf(err, "GetHeader: invalid IAVL height")
-	}
-
-	ctx, err := p.getQueryContext(iavlHeight, false)
+	ctx, err := p.getQueryContext(int64(number), false)
 	if err != nil {
 		return nil, errorslib.Wrap(err, "GetHeader: failed to use query context")
 	}
@@ -68,14 +62,15 @@ func (p *plugin) GetHeaderByNumber(number uint64) (*coretypes.Header, error) {
 	if bz == nil {
 		return nil, errors.New("GetHeader: polaris header not found in kvstore")
 	}
+
 	header, err := coretypes.UnmarshalHeader(bz)
 	if err != nil {
 		return nil, errorslib.Wrap(err, "GetHeader: failed to unmarshal")
 	}
 
-	// if int64(header.Number.Uint64()) != iavlHeight {
-	// 	panic("header number is not equal to the given iavl tree height")
-	// }
+	if header.Number.Uint64() != number {
+		return nil, errorslib.Wrap("GetHeader: header number mismatch, got %d, expected %d", header.Number.Uint64(), number)
+	}
 
 	return header, nil
 }
@@ -88,25 +83,4 @@ func (p *plugin) StoreHeader(header *coretypes.Header) error {
 	}
 	p.ctx.KVStore(p.storekey).Set([]byte{types.HeaderKey}, bz)
 	return nil
-}
-
-// getIAVLHeight returns the IAVL height for the given block number.
-func (p *plugin) getIAVLHeight(number int64) (int64, error) {
-	var iavlHeight int64
-	switch rpc.BlockNumber(number) { //nolint:nolintlint,exhaustive // covers all cases.
-	case rpc.SafeBlockNumber, rpc.FinalizedBlockNumber:
-		iavlHeight = p.ctx.BlockHeight() - 1
-	case rpc.PendingBlockNumber, rpc.LatestBlockNumber:
-		iavlHeight = p.ctx.BlockHeight()
-	case rpc.EarliestBlockNumber:
-		iavlHeight = 1
-	default:
-		iavlHeight = number
-	}
-
-	if iavlHeight < 1 {
-		return 1, fmt.Errorf("invalid block number %d", number)
-	}
-
-	return iavlHeight, nil
 }
