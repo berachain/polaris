eb7aa2c372fedaf037d80f43b5e0d47ca5ec6f5d
arch@berachain.com <arch@berachain.com>
2023-06-01 14:04:02 -0400

    deleted a single space

--- e2e/hive/simulators/rpc/main.go
+++ e2e/hive/simulators/rpc/main.go
@@ -23,7 +23,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-// nolint: unused // might use them in the future for tests not yet implemented
+//nolint: unused // might use them in the future for tests not yet implemented
 package main
 
 import (
