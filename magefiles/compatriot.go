6123f5285e694804f474b0b043b492aef013d283
arch@berachain.com <arch@berachain.com>
2023-06-19 18:17:34 -0400

    remove -it from docker run

--- magefiles/compatriot.go
+++ magefiles/compatriot.go
@@ -59,14 +59,14 @@ func (c Compatriot) BuildWithBase() error {
 func (c Compatriot) Test() error {
 	LogGreen("Running compatriot...")
 
-	return dockerRun("-p", "8545:8545", "-it", "compatriot")
+	return dockerRun("-p", "8545:8545", "compatriot")
 }
 
 // TestV runs the compatriot tests with verbose output.
 func (c Compatriot) TestV() error {
 	LogGreen("Running compatriot with verbose output...")
 
-	return dockerRun("-p", "8545:8545", "-it", "compatriot")
+	return dockerRun("-p", "8545:8545", "compatriot")
 }
 
 // Runs build (without base) and executes the compatriot tests.
