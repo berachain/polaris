4fb7cd2dfda11ddfdbff9261a28144f586382e4a
Cal Bera <calbera@berachain.com>
2023-06-30 16:06:58 -0400

    revert: "fix(staking): Return the empty value instead of error when not found" (#870)
    Reverts berachain/polaris#868
    Geth reflect issues with empty return type, more debugging required

--- cosmos/precompile/staking/helpers.go
+++ cosmos/precompile/staking/helpers.go
@@ -32,7 +32,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
-	generated "pkg.berachain.dev/polaris/contracts/bindings/cosmos/precompile/staking"
 	cosmlib "pkg.berachain.dev/polaris/cosmos/lib"
 	"pkg.berachain.dev/polaris/eth/common"
 )
@@ -215,10 +214,7 @@ func (c *Contract) activeValidatorsHelper(ctx context.Context) ([]any, error) {
 	res, err := c.querier.Validators(ctx, &stakingtypes.QueryValidatorsRequest{
 		Status: stakingtypes.BondStatusBonded,
 	})
-	if status.Code(err) == codes.NotFound {
-		// handle the case where the active validators do not exist
-		return []any{common.Address{}}, nil
-	} else if err != nil {
+	if err != nil {
 		return nil, err
 	}
 
@@ -239,10 +235,7 @@ func (c *Contract) validatorsHelper(ctx context.Context) ([]any, error) {
 	res, err := c.querier.Validators(ctx, &stakingtypes.QueryValidatorsRequest{
 		Status: stakingtypes.BondStatusBonded,
 	})
-	if status.Code(err) == codes.NotFound {
-		// handle the case where the validators do not exist
-		return []any{[]generated.IStakingModuleValidator{}}, nil
-	} else if err != nil {
+	if err != nil {
 		return nil, err
 	}
 
@@ -259,10 +252,7 @@ func (c *Contract) validatorHelper(ctx context.Context, valAddr string) ([]any, 
 	res, err := c.querier.Validator(ctx, &stakingtypes.QueryValidatorRequest{
 		ValidatorAddr: valAddr,
 	})
-	if status.Code(err) == codes.NotFound {
-		// handle the case where the validator does not exist
-		return []any{generated.IStakingModuleValidator{}}, nil
-	} else if err != nil {
+	if err != nil {
 		return nil, err
 	}
 
@@ -280,10 +270,7 @@ func (c *Contract) delegatorValidatorsHelper(ctx context.Context, accAddr string
 	res, err := c.querier.DelegatorValidators(ctx, &stakingtypes.QueryDelegatorValidatorsRequest{
 		DelegatorAddr: accAddr,
 	})
-	if status.Code(err) == codes.NotFound {
-		// handle the case where the delegator validators do not exist
-		return []any{[]generated.IStakingModuleValidator{}}, nil
-	} else if err != nil {
+	if err != nil {
 		return nil, err
 	}
 
