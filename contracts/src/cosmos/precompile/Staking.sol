6ce989813ff2ba8c4b742ee027fcc38ec1361b18
Devon Bear <itsdevbear@berachain.com>
2023-07-06 12:23:27 -0400

    unit tests pass

--- contracts/src/cosmos/precompile/Staking.sol
+++ contracts/src/cosmos/precompile/Staking.sol
@@ -43,11 +43,7 @@ interface IStakingModule {
      * @dev Emitted by the staking module when `amount` tokens are redelegated from
      * `sourceValidator` to `destinationValidator`
      */
-    event Redelegate(
-        address indexed sourceValidator,
-        address indexed destinationValidator,
-        Cosmos.Coin[] amount
-    );
+    event Redelegate(address indexed sourceValidator, address indexed destinationValidator, Cosmos.Coin[] amount);
 
     /**
      * @dev Emitted by the staking module when `amount` tokens are used to create `validator`
@@ -64,10 +60,7 @@ interface IStakingModule {
      * unbonding delegation with `validator`
      */
     event CancelUnbondingDelegation(
-        address indexed validator,
-        address indexed delegator,
-        Cosmos.Coin[] amount,
-        int64 creationHeight
+        address indexed validator, address indexed delegator, Cosmos.Coin[] amount, int64 creationHeight
     );
 
     /////////////////////////////////////// READ METHODS //////////////////////////////////////////
@@ -85,71 +78,56 @@ interface IStakingModule {
     /**
      * @dev Returns the validator at the given address.
      */
-    function getValidator(
-        address validatorAddress
-    ) external view returns (Validator memory);
+    function getValidator(address validatorAddress) external view returns (Validator memory);
 
     /**
      * @dev Returns all the validators delegated to by the given delegator.
      */
-    function getDelegatorValidators(
-        address delegatorAddress
-    ) external view returns (Validator[] memory);
+    function getDelegatorValidators(address delegatorAddress) external view returns (Validator[] memory);
 
     /**
      * @dev Returns the `amount` of tokens currently delegated by `delegatorAddress` to
      * `validatorAddress`
      */
-    function getDelegation(
-        address delegatorAddress,
-        address validatorAddress
-    ) external view returns (uint256);
+    function getDelegation(address delegatorAddress, address validatorAddress) external view returns (uint256);
 
     /**
      * @dev Returns a time-ordered list of all UnbondingDelegationEntries between
      * `delegatorAddress` and `validatorAddress`
      */
-    function getUnbondingDelegation(
-        address delegatorAddress,
-        address validatorAddress
-    ) external view returns (UnbondingDelegationEntry[] memory);
+    function getUnbondingDelegation(address delegatorAddress, address validatorAddress)
+        external
+        view
+        returns (UnbondingDelegationEntry[] memory);
 
     /**
      * @dev Returns a list of `delegatorAddress`'s redelegating bonds from `srcValidator` to
      * `dstValidator`
      */
-    function getRedelegations(
-        address delegatorAddress,
-        address srcValidator,
-        address dstValidator
-    ) external view returns (RedelegationEntry[] memory);
+    function getRedelegations(address delegatorAddress, address srcValidator, address dstValidator)
+        external
+        view
+        returns (RedelegationEntry[] memory);
 
     ////////////////////////////////////// WRITE METHODS //////////////////////////////////////////
 
     /**
      * @dev msg.sender delegates the `amount` of tokens to `validatorAddress`
      */
-    function delegate(
-        address validatorAddress,
-        uint256 amount
-    ) external payable returns (bool);
+    function delegate(address validatorAddress, uint256 amount) external payable returns (bool);
 
     /**
      * @dev msg.sender undelegates the `amount` of tokens from `validatorAddress`
      */
-    function undelegate(
-        address validatorAddress,
-        uint256 amount
-    ) external payable returns (bool);
+    function undelegate(address validatorAddress, uint256 amount) external payable returns (bool);
 
     /**
      * @dev msg.sender redelegates the `amount` of tokens from `srcValidator` to `validtorDstAddr`
      */
-    function beginRedelegate(
-        address srcValidator,
-        address dstValidator,
-        uint256 amount
-    ) external payable returns (bool);
+    function beginRedelegate(address srcValidator, address dstValidator, uint256 amount)
+        external
+        payable
+        returns (bool);
 
     /**
      * @dev Cancels msg.sender's unbonding delegation with `validatorAddress` and delegates the
@@ -157,11 +135,10 @@ interface IStakingModule {
      *
      * Provide the `creationHeight` of the original unbonding delegation
      */
-    function cancelUnbondingDelegation(
-        address validatorAddress,
-        uint256 amount,
-        int64 creationHeight
-    ) external payable returns (bool);
+    function cancelUnbondingDelegation(address validatorAddress, uint256 amount, int64 creationHeight)
+        external
+        payable
+        returns (bool);
 
     //////////////////////////////////////////// UTILS ////////////////////////////////////////////
 
