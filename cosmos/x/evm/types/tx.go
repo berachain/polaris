ee2827342540d25afcd5771ceeaf1be25d54a65d
Devon Bear <itsdevbear@berachain.com>
2023-07-28 15:18:18 +0200

    spacing

--- cosmos/x/evm/types/tx.go
+++ cosmos/x/evm/types/tx.go
@@ -155,7 +155,6 @@ func GetAsEthTx(tx sdk.Tx) *coretypes.Transaction {
 // ProvideEthereumTransactionGetSigners defines a custom function for
 // utilizing custom signer handling for `WrappedEthereumTransaction`s
 func ProvideEthereumTransactionGetSigners() signing.CustomGetSigner {
-
 	// Utilize a sync pool to reduce memory usage.
 	var txSyncPool = sync.Pool{
 		New: func() any { return new(coretypes.Transaction) },
