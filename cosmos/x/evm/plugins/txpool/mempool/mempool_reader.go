f29f9f28e369af9da2fb8c3acd34592ff73955d9
Devon Bear <itsdevbear@berachain.com>
2023-08-17 18:35:41 -0400

    comments

--- cosmos/x/evm/plugins/txpool/mempool/mempool_reader.go
+++ cosmos/x/evm/plugins/txpool/mempool/mempool_reader.go
@@ -38,6 +38,8 @@ func (etp *EthTxPool) Get(hash common.Hash) *coretypes.Transaction {
 }
 
 // Pending is called when txs in the mempool are retrieved.
+//
+// NOT THREAD SAFE
 func (etp *EthTxPool) Pending(bool) map[common.Address]coretypes.Transactions {
 	pendingNonces := make(map[common.Address]uint64)
 	pending := make(map[common.Address]coretypes.Transactions)
@@ -76,6 +78,8 @@ func (etp *EthTxPool) Pending(bool) map[common.Address]coretypes.Transactions {
 }
 
 // queued retrieves the content of the mempool.
+//
+// NOT THREAD SAFE
 func (etp *EthTxPool) queued() map[common.Address]coretypes.Transactions {
 	pendingNonces := make(map[common.Address]uint64)
 	queued := make(map[common.Address]coretypes.Transactions)
@@ -116,6 +120,8 @@ func (etp *EthTxPool) queued() map[common.Address]coretypes.Transactions {
 
 // Nonce returns the nonce for the given address from the mempool if the address has sent a tx
 // in the mempool.
+//
+// NOT THREAD SAFE
 func (etp *EthTxPool) Nonce(addr common.Address) uint64 {
 	pendingNonces := make(map[common.Address]uint64)
 
@@ -155,6 +161,8 @@ func (etp *EthTxPool) Nonce(addr common.Address) uint64 {
 }
 
 // Stats returns the number of currently pending and queued (locally created) transactions.
+//
+// NOT THREAD SAFE
 func (etp *EthTxPool) Stats() (int, int) {
 	var pendingTxsLen, queuedTxsLen int
 	pending, queued := etp.Content()
@@ -170,6 +178,8 @@ func (etp *EthTxPool) Stats() (int, int) {
 
 // ContentFrom retrieves the data content of the transaction pool, returning the pending as well as
 // queued transactions of this address, grouped by nonce.
+//
+// NOT THREAD SAFE
 func (etp *EthTxPool) ContentFrom(addr common.Address) (coretypes.Transactions, coretypes.Transactions) {
 	pending, queued := etp.Content()
 	return pending[addr], queued[addr]
@@ -177,6 +187,8 @@ func (etp *EthTxPool) ContentFrom(addr common.Address) (coretypes.Transactions, 
 
 // Content retrieves the data content of the transaction pool, returning all the pending as well as
 // queued transactions, grouped by account and nonce.
+//
+// NOT THREAD SAFE
 func (etp *EthTxPool) Content() (
 	map[common.Address]coretypes.Transactions, map[common.Address]coretypes.Transactions,
 ) {
