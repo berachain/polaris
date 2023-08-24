8cd61cf3f298dc078d9905f37cb373bb42eeb435
arch@berachain.com <arch@berachain.com>
2023-08-24 12:25:00 -0400

    added cons address codec to validator store interface

--- cosmos/precompile/staking/staking.go
+++ cosmos/precompile/staking/staking.go
@@ -46,6 +46,8 @@ import (
 type ValidatorStore interface {
 	ValidatorAddressCodec() address.Codec
 	ValidatorByConsAddr(ctx context.Context, addr sdk.ConsAddress) (stakingtypes.ValidatorI, error)
+
+	ConsensusAddressCodec() address.Codec
 }
 
 // Contract is the precompile contract for the staking module.
