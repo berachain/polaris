c7d91b97aab284004e83bc80a97ccb16f6d72564
Devon Bear <itsdevbear@berachain.com>
2023-09-01 12:57:52 -0400

    rebring test

---
+++ contracts/src/testing/PrecompileConstructor.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2023 Berachain Foundation
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+pragma solidity >=0.8.4;
+
+import {IERC20} from "../../lib/IERC20.sol";
+import {IStakingModule} from "../cosmos/precompile/Staking.sol";
+
+// An example of calling a precompile from the contract's constructor.
+contract PrecompileConstructor {
+    IStakingModule public immutable stakingModule = IStakingModule(0xd9A998CaC66092748FfEc7cFBD155Aae1737C2fF);
+
+    constructor() {
+        // should not revert
+        stakingModule.getValidator(address(0x0));
+    }
+}
