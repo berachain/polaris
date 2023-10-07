4a587ed1be7f655be41503ad853136f2cf3e8c43
Devon Bear <itsdevbear@berachain.com>
2023-10-07 19:57:02 -0400

    chore(fmt): formatting

--- eth/accounts/hd.go
+++ eth/accounts/hd.go
@@ -20,27 +20,14 @@
 
 package accounts
 
+import (
+	"github.com/ethereum/go-ethereum/accounts"
+)
+
 var (
 	// Bip44CoinType was defined by EIP-84 (https://github.com/ethereum/EIPs/issues/84).
 	Bip44CoinType uint32 = 60
 
-	// EtherDecimals is the number of decimal places in an Ether.
-	EtherDecimals int64 = 18
-
 	// BIP44HDPath is the default BIP44 HD path used on Ethereum.
-	BIP44HDPath = DefaultBaseDerivationPath.String()
+	BIP44HDPath = accounts.DefaultBaseDerivationPath.String()
 )
-
-// HDPathIterator creates a generic iterator for a given base path.
-func NewHDPathIterator(basePath string, ledgerIter bool) (HDPathIterator, error) {
-	hdPath, err := ParseDerivationPath(basePath)
-	if err != nil {
-		return nil, err
-	}
-
-	if ledgerIter {
-		return LedgerLiveIterator(hdPath), nil
-	}
-
-	return DefaultIterator(hdPath), nil
-}
