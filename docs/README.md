a7d688facff2ea28f9414dbdf5ef5c110790c59a
Devon Bear <itsdevbear@berachain.com>
2023-10-17 11:48:50 -0400

    refactor(docs): move to `docs-monorepo` (#1232)

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# @cordoruy pls add a readme and stuff explaining how the docs compiling works
--- docs/web/window.d.ts
+++
@@ -1,6 +0,0 @@
-import { Window as KeplrWindow } from '@keplr-wallet/types';
-
-declare global {
-  // eslint-disable-next-line
-  interface Window extends KeplrWindow {}
-}
