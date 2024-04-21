0bfc62b611c5543a3d5984ca215814e3742a1176
Vlad Tereshyn <vladyslav.tereshyn@gmail.com>
2024-04-21 14:09:37 +0300

    chore: Fix typo: change 'govered' to 'governed'

--- cosmos/api/polaris/evm/module/v1alpha1/module.pulsar.go
+++ cosmos/api/polaris/evm/module/v1alpha1/module.pulsar.go
@@ -372,7 +372,7 @@ func (x *fastReflection_Module) ProtoMethods() *protoiface.Methods {
 // SPDX-License-Identifier: BUSL-1.1
 //
 // Copyright (C) 2023, Berachain Foundation. All rights reserved.
-// Use of this software is govered by the Business Source License included
+// Use of this software is governed by the Business Source License included
 // in the LICENSE file of this repository and at www.mariadb.com/bsl11.
 //
 // ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
